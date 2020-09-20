366b0c73700e3e13200db4166ce2ecbdc69e6de2
arthuro1115 <58000481+arthuro1115@users.noreply.github.com>
2020-09-20 16:00:36 -0300

    Update PID.py

--- PID.py
+++ PID.py
@@ -5,18 +5,35 @@ kP = 0.5
 kI = None
 kD = None
 
+Datual {5, 7 ,20 ,30 ,35 ,10 , 50 , 80}
+
 ##VARIAVEIS
 erro =  None
-dAtual = 20
+integral = None
+lasterror = None
 
 P = None
 I = None
+D = None
+PID = None 
+
 
 for x in range (10):
+    ##Proporcional
     erro = dAtual-setpoint
     P = kP * erro
-    dAtual = P  - setpoint
     
+    ##Integral
+    Integral  = Integral + erro
+    I = Integral * kI
+    
+    ##Derivativo
+    erro = lasterro - erro
+    D = erro * kD
+    
+    #PID SOMATORIA
+    PID = P + I + D
+
     print("erro")
     print(erro)
     print("P")
